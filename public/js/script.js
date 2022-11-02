13a10fdbacbe354fb98230bf27f427322124b6cf
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2022-11-01 20:59:31 -0400

    RM: Imported PixiJS library; added resizable canvas (empty with black bg); set dflt css

--- public/js/script.js
+++ public/js/script.js
@@ -8,4 +8,50 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+// Create a PIXI canvas
+// For nice resolution on circle, source: https://stackoverflow.com/questions/41932258/how-do-i-antialias-graphics-circle-in-pixijs
+let app = new PIXI.Application({
+    transparent: false,
+    // width: 999, //640
+    // height: 487, //360
+    width: window.innerWidth,
+    height: window.innerHeight,
+    autoResize: true,
+    // resolution: devicePixelRatio,
+    antialias: true,
+});
+document.body.appendChild(app.view);
+
+// // Lets create a red square, this isn't 
+// // necessary only to show something that can be position
+// // to the bottom-right corner
+// const rect = new PIXI.Graphics()
+//     .beginFill(0xff0000)
+//     .drawRect(-100, -100, 100, 100);
+
+// // Add it to the stage
+// app.stage.addChild(rect);
+
+
+/********************************
+ * RESIZE DOCUMENT AUTOMATICALLY
+ * https://jsfiddle.net/bigtimebuddy/oaLwp0p9/
+********************************/
+
+
+// Listen for window resize events
+window.addEventListener('resize', resize);
+
+// Resize function window
+function resize() {
+    // Resize the renderer
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+
+    // You can use the 'screen' property as the renderer visible
+    // area, this is more useful than view.width/height because
+    // it handles resolution
+    rect.position.set(app.screen.width, app.screen.height);
+}
+
+resize();
+// end resize document
